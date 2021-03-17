3f3fd2f0a2f8c592d4df57f934e312b9b5663621
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-03-17 14:54:27 +0100

    first commit

---
+++ e-commerce-projectwork/src/main/java/it/karasho/ECommerceProjectworkApplication.java
@@ -0,0 +1,13 @@
+package it.karasho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjectworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjectworkApplication.class, args);
+	}
+
+}
---
+++ e-commerce-projectwork/src/test/java/it/karasho/ECommerceProjectworkApplicationTests.java
@@ -0,0 +1,13 @@
+package it.karasho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjectworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
