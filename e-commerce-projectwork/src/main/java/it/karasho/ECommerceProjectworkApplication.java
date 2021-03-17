b56970d3dede249d1f145f717f81c6c8bfdccc2a
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-03-17 15:23:43 +0100

    main

--- e-commerce-projectwork/src/main/java/it/karasho/ECommerceProjectworkApplication.java
+++ e-commerce-projectwork/src/main/java/it/karasho/ECommerceProjectworkApplication.java
@@ -1,9 +1,13 @@
 package it.karasho;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan(basePackages = "it.gestionalejaclsg.jac.entity")
 public class ECommerceProjectworkApplication {
 
 	public static void main(String[] args) {
